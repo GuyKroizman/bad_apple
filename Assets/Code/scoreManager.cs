b142e2598a6adf5a0f57ec488ba4663f9c220b67
reahaas <rea@scadafence.com>
2022-07-22 15:21:36 +0300

    Added score

---
+++ Assets/Code/scoreManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class scoreManager : MonoBehaviour
+{
+
+    private float lastUpdate;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastUpdate = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Time.time - lastUpdate >= 1f)
+        {
+            score += 1;
+            this.GetComponent<UnityEngine.UI.Text>().text = score.ToString();
+            lastUpdate = Time.time;
+        }
+    }
+}
